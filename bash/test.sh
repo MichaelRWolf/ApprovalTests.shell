6532a99873088334fcf680532d379594a750d901
Michael R. Wolf <MichaelRWolf@att.net>
2025-02-03 14:02:59 -0500

    @ t New test: diff_tool_selection

--- bash/test.sh
+++ bash/test.sh
@@ -10,3 +10,11 @@ echo "test: fails and triggers diff tool"
 echo ""
 
 ./verify.sh -d meld <<<"Input to test without a name"
+
+cat <<DIFF_TOOL_SELECTION | ./verify.sh -t diff_tool_selection -d 'pr -m -t'
+# Logic for diff tool
+# 1. --tool argument takes top priority.
+# 2. If no --tool is specified, GIT_DIFF_TOOL is checked next.
+# 3. If GIT_DIFF_TOOL is unset, git config --get diff.tool is used.
+# 4. If neither is set, Git falls back to its internal diff.
+DIFF_TOOL_SELECTION
